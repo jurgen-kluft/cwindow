66aae2e4ecddcb9b013f6e9194313f57aa78e335
jurgen.kluft <jurgen.kluft@gmail.com>
2026-07-31 21:03:53 +0800

    * adjust to changes in ccode

--- package/package.go
+++ package/package.go
@@ -1,9 +1,9 @@
 package cwindow
 
 import (
-	denv "github.com/jurgen-kluft/ccode/denv"
 	ccore "github.com/jurgen-kluft/ccore/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
+	denv "github.com/jurgen-kluft/gide/denv"
 )
 
 const (
